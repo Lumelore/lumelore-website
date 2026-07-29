28dc2420a39a1a0fc4c9262b8af0be0e0dc4932b
Lumelore <lumelore@proton.me>
2026-07-29 01:43:22 -0500

    remove old code from before I was using local storage

--- src/scripts/save-theme.ts
+++ src/scripts/save-theme.ts
@@ -1,12 +1,5 @@
 const colorThemes: NodeListOf<HTMLInputElement> = document.querySelectorAll('[name="theme-option"]');
 
-// No longer using - cookies are not needed
-const cookieStoreTheme = function (theme: string) {
-    try {
-        document.cookie = `theme=${theme}; Max-Age=604800; Path=/; Secure;`;
-    }
-    catch (e) { console.error("Unable to save theme as cookie.") }
-}
 
 const localStoreTheme = function (theme: string) {
     try {
@@ -16,23 +9,6 @@ const localStoreTheme = function (theme: string) {
 }
 
 const setTheme = function () {
-
-    // Uncomment below and comment the const theme line to go back to the old cookie way
-    // click event for buttons will need to be changed back to cookie store theme as well
-
-    /*
-    // Get the cookie theme
-    const cookieTheme = document.cookie.split(";").find((row) => row.startsWith("theme="))?.split("=")[1];
-    let theme: string | undefined | null = cookieTheme;
-
-    // If null grab from local storage
-    if (cookieTheme === undefined) {
-        theme = localStorage.getItem("theme");
-        // store local storage theme in cookie
-        if (theme !== null) { 
-            cookieStoreTheme(theme) 
-        }
-    }*/
 
     const theme = localStorage.getItem("theme");
 
